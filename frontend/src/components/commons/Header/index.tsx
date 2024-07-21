350cd7f1b8959916d8b1eebbed8863ac9c1900cb
Constanca Junqueira <constancajunqueira@MacBook-Air-de-Constanca.local>
2024-07-21 19:21:36 -0300

    refactor: color button navbar

--- frontend/src/components/commons/Header/index.tsx
+++ frontend/src/components/commons/Header/index.tsx
@@ -6,16 +6,16 @@ export const Header = () => {
     return (
         <>
             <Flex minWidth='max-content' p='16px' alignItems='center' gap='2' borderBottom="1px" borderBottomColor="neutral.300">
-                <Flex alignItems='center'justify="space-around">
+                <Flex alignItems='center' justify="space-around">
                     <Logo width="32" height="32" />
-                    <Heading color="neutral.400"  marginX="8px" size='md'>Cidade Ativa</Heading>
+                    <Heading color="neutral.400" marginX="8px" size='md'>Cidade Ativa</Heading>
                 </Flex>
                 <Spacer />
                 <ButtonGroup gap='4'>
                     <Button fontSize="16px" variant='link' color="green.200">Mapas</Button>
                     <Button fontSize="16px" variant='link' color="green.200">Pesquisar</Button>
                     <Button fontSize="16px" variant='link' color="green.200">Sobre</Button>
-                    <Button fontSize="16px" borderRadius="20px" colorScheme='teal'>Login</Button>
+                    <Button fontSize="16px" borderRadius="20px" bg="green.200" color="neutral.100" borderColor="green.200">Login</Button>
                 </ButtonGroup>
             </Flex>
         </>
