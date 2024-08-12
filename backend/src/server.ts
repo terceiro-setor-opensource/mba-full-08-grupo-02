1a5b64295f3aa648124c81f0229ee8ee2ff1e0fd
rafael.baptista <rafael.sampaio@aluno.faculdadeimpacta.com.br>
2024-08-12 19:53:14 -0300

    fix: refactor code supabase

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,16 @@
 import express from 'express';
-import { Request, Response } from 'express';
 import userRouter from './routes';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(userRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello Word")
+  res.status(200).json({ status : 'success', version: '1.0.0'})
 })
-
-
-app.use('/users', userRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
