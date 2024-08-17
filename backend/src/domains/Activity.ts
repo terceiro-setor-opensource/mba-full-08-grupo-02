01988d3160c2300e1f7c4119bdf52198102c8f51
lumamontes <lumagoesmontes@gmail.com>
2024-08-17 19:14:01 -0300

    feat: add endpoints for fetching activities and associated benefits

---
+++ backend/src/domains/Activity.ts
@@ -0,0 +1,6 @@
+export type ActivityResponse = {
+  id: number;
+  name: string;
+  description: string;
+};
+
---
+++ backend/src/domains/Benefit.ts
@@ -0,0 +1,5 @@
+export type BenefitResponse = {
+  id: number;
+  name: string;
+  description: string;
+};
