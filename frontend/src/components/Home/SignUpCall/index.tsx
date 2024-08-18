468fe2a65beee6cff0714591fb52a586233e8997
Constanca Junqueira <constancajunqueira@MacBook-Air-de-Constanca.local>
2024-08-18 13:28:43 -0300

    feat: signUp layout page

--- frontend/src/components/Home/SignUpCall/index.tsx
+++ frontend/src/components/Home/SignUpCall/index.tsx
@@ -1,24 +1,40 @@
-import { Box, HStack } from "@chakra-ui/layout"
+import { Box, HStack } from '@chakra-ui/layout'
 import { Button, Hide, Image, Text } from '@chakra-ui/react'
 import LandingPage3 from '../../../assets/images/landing3.png'
-
+import { useNavigate } from 'react-router-dom'
 
 export const SignUpCall = () => {
-    return (
-        <HStack alignItems="flex-start" justifyContent={"space-around"} p={10}>
-        <Box>
-            <Text textStyle={'h1'} mb={12}>Pronto para essa jornada?</Text>
-            <Button padding={20} cursor="pointer" borderRadius="20px" bg="green.200" color="neutral.100" borderColor="green.200">Cadastre-se</Button>
-        </Box>
-        <Hide below='md'>
-            <Image
-                maxW="50%"
-                borderRadius="8px"
-                objectFit='cover'
-                src={LandingPage3}
-                alt='homem correndo na rua'
-            />
-        </Hide>
+    const navigate = useNavigate()
+    
+  return (
+    <HStack alignItems="flex-start" justifyContent={'space-around'} p={10}>
+      <Box>
+        <Text textStyle={'h1'} mb={12}>
+          Pronto para essa jornada?
+        </Text>
+        <Button
+          padding={20}
+          cursor="pointer"
+          borderRadius="20px"
+          bg="green.200"
+          color="neutral.100"
+          borderColor="green.200"
+          onClick={() => {
+            navigate("/cadastro")
+          }}
+        >
+          Cadastre-se
+        </Button>
+      </Box>
+      <Hide below="md">
+        <Image
+          maxW="50%"
+          borderRadius="8px"
+          objectFit="cover"
+          src={LandingPage3}
+          alt="homem correndo na rua"
+        />
+      </Hide>
     </HStack>
-    )
+  )
 }
--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,5 +1,56 @@
+import {
+  Button,
+  Checkbox,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+} from '@chakra-ui/react'
+import { Stack, VStack } from '@chakra-ui/layout'
+import { useState } from 'react'
+
 export const SignUp = () => {
-    return (
-        <div>Register</div>
-    )
+  const [show, setShow] = useState<boolean>(false)
+
+  return (
+    <VStack height={'100vh'} pt={36}>
+      <Stack w={{ base: '90%', md: '80%', lg: '40%' }}>
+        <VStack pb={20} alignItems="flex-start">
+          <Text textStyle={'h1'}>Crie sua conta!</Text>
+          <Text textStyle={'subtitle'} textColor="neutral.400">
+            É gratuito e simples!
+          </Text>
+        </VStack>
+        <VStack gap={28} pb={20}>
+          <FormControl isRequired>
+            <FormLabel mt={12}>Nome completo</FormLabel>
+            <Input size="lg" placeholder="Nome completo" />
+            <FormLabel mt={12}>Email</FormLabel>
+            <Input type="email" size="lg" placeholder="Email" />
+            <FormLabel mt={12}>Senha</FormLabel>
+            <InputGroup size="lg">
+              <Input type={show ? 'text' : 'password'} placeholder="Senha" />
+              <InputRightElement width="4.5rem">
+                <Button h="1.75rem" onClick={() => setShow(!show)}>
+                  {show ? 'Hide' : 'Show'}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+            <Checkbox mt={12}>Concordo com os Termos e Condições</Checkbox>
+          </FormControl>
+          <Button
+            borderRadius="20px"
+            w="100%"
+            bg="purple.200"
+            color="neutral.100"
+            borderColor="purple.200"
+          >
+            Cadastrar
+          </Button>
+        </VStack>
+      </Stack>
+    </VStack>
+  )
 }
