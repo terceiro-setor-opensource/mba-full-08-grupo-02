835e3701f5883f6444715921be3530bf8add2a7f
lumamontes <lumagoesmontes@gmail.com>
2024-09-02 03:47:57 -0300

    chore: correct typing for components

--- frontend/src/components/commons/SearchBar/index.tsx
+++ frontend/src/components/commons/SearchBar/index.tsx
@@ -1,30 +1,25 @@
-import {
-  Input,
-  InputGroup,
-  InputLeftElement,
-  InputProps,
-} from "@chakra-ui/react";
-import { BiSearch } from "react-icons/bi";
+import { Input, InputGroup, InputLeftElement, InputProps } from '@chakra-ui/react'
+import { BiSearch } from 'react-icons/bi'
 
 type SearchBarProps = InputProps & {
-  placeholder?: string;
-  onSearch: (value: string) => void;
+  placeholder?: string
+  onSearch: (value: string) => void
 }
 
-export const SearchBar = ({placeholder, onSearch, ...props}: SearchBarProps) => {
+export const SearchBar = ({ placeholder, onSearch, children, ...props }: SearchBarProps) => {
   return (
     <>
       <InputGroup borderRadius={5} size="md">
-        <InputLeftElement
-          pointerEvents="none"
-          children={<BiSearch color="gray.600" />}
-        />
-        <Input type="text" 
-          placeholder={placeholder} 
-          onChange={(e) => onSearch(e.target.value)} 
-          {...props} 
+        <InputLeftElement pointerEvents="none">
+          <BiSearch color="gray.600" />
+        </InputLeftElement>
+        <Input
+          type="text"
+          placeholder={placeholder}
+          onChange={(e) => onSearch(e.target.value)}
+          {...props}
         />
       </InputGroup>
     </>
-  );
-};
+  )
+}
--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -1,4 +1,4 @@
-import { ChakraTheme, extendTheme, type ThemeConfig } from "@chakra-ui/react"
+import { ChakraTheme, Colors, extendTheme, type ThemeConfig } from '@chakra-ui/react'
 
 const config: ThemeConfig = {
   initialColorMode: 'light',
@@ -6,48 +6,48 @@ const config: ThemeConfig = {
 }
 
 const space = {
-    4: '0.25rem',
-    12: '0.75rem',
-    20: '1.25rem',
-    28: '1.75rem',
-    36: '2.25rem',
-    44: '2.75rem',
-    52: '3.25rem',
-    60: '3.75rem'
+  4: '0.25rem',
+  12: '0.75rem',
+  20: '1.25rem',
+  28: '1.75rem',
+  36: '2.25rem',
+  44: '2.75rem',
+  52: '3.25rem',
+  60: '3.75rem',
 }
 
 const textStyles = {
   h1: {
-    fontSize: "1.5rem",
-    fontWeight: "semibold",
+    fontSize: '1.5rem',
+    fontWeight: 'semibold',
   },
   h2: {
-    fontSize: "1.125rem",
+    fontSize: '1.125rem',
     fontWeight: 'semibold',
   },
   subtitle: {
-    fontSize: "1.25rem",
+    fontSize: '1.25rem',
     fontWeight: 'regular',
   },
   bodyFirst: {
-    fontSize: "1rem",
+    fontSize: '1rem',
     fontWeight: 'regular',
   },
   paragraphBold: {
-    fontSize: "1rem",
+    fontSize: '1rem',
     fontWeight: 'bold',
   },
   bodySecond: {
-    fontSize: "0.875rem",
+    fontSize: '0.875rem',
     fontWeight: 'regular',
   },
   caption: {
-    fontSize: "0.688rem",
+    fontSize: '0.688rem',
     fontWeight: 'semibold',
   },
   button: {
-    fontSize: "1rem"
-  }
+    fontSize: '1rem',
+  },
 }
 
 const colors = {
@@ -56,43 +56,43 @@ const colors = {
   },
   purple: {
     200: '#AC46F0',
-    100: '#675970'
+    100: '#675970',
   },
   green: {
     100: '#46F08F',
-    200: '#609B7A'
+    200: '#609B7A',
   },
   neutral: {
     500: '#000000',
     400: '#777E90',
     300: '#D4D7E1',
-    200: "#DEE5ED",
-    100: "#FFFFFF"
+    200: '#DEE5ED',
+    100: '#FFFFFF',
   },
   status: {
-    info: "#1D73C9",
-    warning: "#E39803",
-    success: "#1DA555",
-    error: "#C42231"
-  }
+    info: '#1D73C9',
+    warning: '#E39803',
+    success: '#1DA555',
+    error: '#C42231',
+  },
 }
 
 const styles = {
   global: {
     'html, body': {
       fontFamily: `'Source Sans 3', sans-serif`,
-    }
-  }
+    },
+  },
 }
 
+export type CustomColors = typeof colors & Colors
+
 interface CustomTheme extends ChakraTheme {
-  textStyles: typeof textStyles,
-  colors: typeof colors,
+  textStyles: typeof textStyles
+  colors: CustomColors
   space: typeof space
 }
 
 const theme = extendTheme({ colors, textStyles, config, styles, space }) as CustomTheme
 
 export default theme
-
-
