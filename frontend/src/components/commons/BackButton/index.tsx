69b1248fa63f7da2f87e80ea305c41302297cecb
Robert <robert.alexandrea7@gmail.com>
2024-10-29 22:11:05 -0300

    fixing back button

--- frontend/src/components/commons/BackButton/index.tsx
+++ frontend/src/components/commons/BackButton/index.tsx
@@ -1,16 +1,24 @@
-import { Text, Link, Flex } from '@chakra-ui/react'
+import { Text, Link, Flex, Button } from '@chakra-ui/react'
 import { t } from 'i18next'
 import { RiArrowLeftLine } from 'react-icons/ri'
+import { useNavigate } from 'react-router-dom'
 
 export const BackButton = () => {
+  const navigate = useNavigate()
   return (
-    <Link href="/.." width="100%">
+    <Button
+      bg="none"
+      onClick={(e) => {
+        e.preventDefault()
+        navigate(-1)
+      }}
+    >
       <Flex alignItems="center">
         <RiArrowLeftLine />
         <Text fontWeight="bold" marginLeft="1rem">
           {t('backButton')}
         </Text>
       </Flex>
-    </Link>
+    </Button>
   )
 }
