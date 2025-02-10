267f6349d52f0b41ad26c12bdf75025a9a1607c7
lumamontes <lumagoesmontes@gmail.com>
2025-02-10 18:38:20 -0300

    fix: add correct linting

--- frontend/src/components/Dashboard/LoadingPlaceSkeleton/details.tsx
+++ frontend/src/components/Dashboard/LoadingPlaceSkeleton/details.tsx
@@ -3,12 +3,9 @@ import { Stack, SimpleGrid, Skeleton, SkeletonText, VStack, Flex, HStack, Badge,
 export const PlaceDetailsSkeleton = () => {
   return (
     <Stack spacing={8} width="100%">
-      {/* Image & Info Section */}
       <SimpleGrid columns={{ base: 1, md: 1, lg: 2 }} spacing={12}>
-        {/* Image Skeleton */}
         <Skeleton height="300px" width="100%" rounded="md" />
 
-        {/* Details Skeleton */}
         <Stack spacing={4} justifyContent="space-between" height="100%">
           <VStack alignItems="flex-start">
             <Flex justifyContent="space-between" width="100%">
@@ -29,10 +26,8 @@ export const PlaceDetailsSkeleton = () => {
         </Stack>
       </SimpleGrid>
 
-      {/* Description */}
       <SkeletonText noOfLines={4} spacing={4} />
 
-      {/* Events Section */}
       <Stack spacing={4}>
         <Skeleton height="30px" width="200px" />
         <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={6}>
@@ -42,14 +37,12 @@ export const PlaceDetailsSkeleton = () => {
         </SimpleGrid>
       </Stack>
 
-      {/* Benefits Section */}
       <Stack spacing={4}>
         <Skeleton height="30px" width="250px" />
         <SkeletonText noOfLines={3} />
         <Skeleton height="40px" width="120px" />
       </Stack>
 
-      {/* Feedback Section */}
       <Stack spacing={4}>
         <Skeleton height="30px" width="250px" />
         <Button isDisabled bg="gray.300" height="50px" width="100%">
