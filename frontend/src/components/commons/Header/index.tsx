458dd358aefefea0e034a02cda7b6299854ce1b2
Robert Alexandre <robert.alexandrea7@gmail.com>
2025-02-13 19:45:03 -0300

    Trocado redirecionamento do botão pesquisar para a tela de places e removido botão mapas

--- frontend/src/components/commons/Header/index.tsx
+++ frontend/src/components/commons/Header/index.tsx
@@ -60,15 +60,12 @@ const LargeScreenMenu: React.FC<LargeScreenMenuProps> = ({
   logout,
 }) => (
   <ButtonGroup gap={12}>
-    <Button cursor="pointer" fontSize={'button'} variant="link">
-      {t('maps')}
-    </Button>
     <Button
       cursor="pointer"
       fontSize={'button'}
       variant="link"
       color="neutral.400"
-      onClick={() => navigate('/dashboard')}
+      onClick={() => navigate('/places')}
     >
       {t('search')}
     </Button>
@@ -162,7 +159,6 @@ export const Header: React.FC = () => {
             variant="outline"
           />
           <MenuList>
-            <MenuItem onClick={handleNavigate('/maps')}>{t('maps')}</MenuItem>
             <MenuItem onClick={handleNavigate('/search')}>
               {t('search')}
             </MenuItem>
